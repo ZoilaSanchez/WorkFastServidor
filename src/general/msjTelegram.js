72488b5ef9e16c7c5b8fa8ca017a7e8c74bf68db
ZoilaSanchez <38706142+ZoilaSanchez@users.noreply.github.com>
2022-10-23 01:22:20 -0600

    Actualizacion Bot

--- src/general/msjTelegram.js
+++ src/general/msjTelegram.js
@@ -4,16 +4,16 @@ const TelegramBot = require('node-telegram-bot-api');
 
 usarCtrl.enviarMsj= (telegramIDS,unidades,nombre) => {
 // Creamos una constante que guarda el Token de nuestro Bot de Telegram que previamente hemos creado desde el bot @BotFather
-var bot = new TelegramBot(process.env.TELEGRAM,{polling: true});
-console.log("proceso enviado ..............")
-console.log(telegramIDS)
-console.log("enviamos..................")
-    for (var i = 0; i < telegramIDS.length; i++) {
-        console.log("Enviando a - ",telegramIDS[i]["chatId"])
-        var chatTelegram = telegramIDS[i]["chatId"]
-        bot.sendMessage(chatTelegram, "Realiza el pedido del producto *" + nombre + "*. En existencias se tienen "+ unidades+".");
-    }
-bot.close
+    const bot = new TelegramBot('5691256241:AAH5L_3KHYJWeNQuK03fKNovKR3cd0_oKR0',{polling: true});
+    console.log("proceso enviado ..............")
+    console.log(telegramIDS)
+    console.log("enviamos..................")
+        for (var i = 0; i < telegramIDS.length; i++) {
+            console.log("Enviando a - ",telegramIDS[i]["chatId"])
+            var chatTelegram = telegramIDS[i]["chatId"]
+            bot.sendMessage(chatTelegram, "Realiza el pedido del producto *" + nombre + "*. En existencias se tienen "+ unidades+".");
+        }
+    bot.close
 
 }
  module.exports = usarCtrl;
